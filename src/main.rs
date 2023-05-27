36135ac5a943ca5729f8cd93d1e23109f8ae4d60
Kerollmops <clement@meilisearch.com>
2023-05-27 15:59:37 +0200

    Good working version, missing edition

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::mem;
-use std::ops::Deref;
 
 use crate::escaped_entry::EscapedEntry;
-use eframe::egui;
+use eframe::egui::{self, InnerResponse};
 use egui::Color32;
 use egui_extras::{Column, TableBuilder};
+use egui_tiles::Container;
+use egui_tiles::Tile;
 use heed::types::ByteSlice;
 use heed::{Database, Env, EnvOpenOptions};
 use once_cell::sync::OnceCell;
@@ -26,134 +27,114 @@ fn main() -> anyhow::Result<()> {
 
     if let Some(env_path) = FileDialog::new().pick_folder() {
         let env = EnvOpenOptions::new().max_dbs(1000).open(env_path)?;
-        let _ = ENV.set(env.clone());
+        let _ = ENV.set(env);
 
-        eframe::run_native(
-            "LMDB Editor",
-            options,
-            Box::new(|ctx| Box::new(LmdbEditor::new(env, ctx))),
-        )
-        .unwrap();
+        eframe::run_native("LMDB Editor", options, Box::new(|ctx| Box::new(LmdbEditor::new(ctx))))
+            .unwrap();
     }
 
     Ok(())
 }
 
 struct LmdbEditor {
-    env: Env,
-    database: (Option<String>, Database<ByteSlice, ByteSlice>),
-    database_to_open: String,
-    entry_to_insert: EscapedEntry,
     wtxn: Option<heed::RwTxn<'static>>,
+    tree: egui_tiles::Tree<Pane>,
 }
 
 impl LmdbEditor {
-    fn new(env: Env, _cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
 
         // TODO do not try to create the database here.
+        let env = ENV.wait();
         let mut wtxn = env.write_txn().unwrap();
         let main_db = env.create_database(&mut wtxn, None).unwrap();
         wtxn.commit().unwrap();
-        LmdbEditor {
-            env,
-            database: (None, main_db),
-            database_to_open: String::new(),
+
+        let mut tiles = egui_tiles::Tiles::default();
+        let mut tabs = vec![];
+
+        // tabs.push({
+        //     let children = (0..7)
+        //         .map(|_| tiles.insert_pane(Pane::OpenNew { database_to_open: String::new() }))
+        //         .collect();
+        //     tiles.insert_horizontal_tile(children)
+        // });
+        // tabs.push({
+        //     let cells = (0..11)
+        //         .map(|_| tiles.insert_pane(Pane::OpenNew { database_to_open: String::new() }))
+        //         .collect();
+        //     tiles.insert_grid_tile(cells)
+        // });
+
+        tabs.push(tiles.insert_pane(Pane::Editable {
+            database_name: None,
+            database: main_db,
             entry_to_insert: EscapedEntry::default(),
-            wtxn: None,
-        }
+        }));
+        tabs.push(tiles.insert_pane(Pane::OpenNew { database_to_open: String::new() }));
+        let root = tiles.insert_tab_tile(tabs);
+        let tree = egui_tiles::Tree::new(root, tiles);
+
+        LmdbEditor { wtxn: None, tree }
     }
 }
 
 impl eframe::App for LmdbEditor {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::Window::new("Edit or Insert an Entry").default_pos([720.0, 480.0]).show(ctx, |ui| {
-            ui.style_mut().spacing.interact_size.y = 0.0; // hack to make `horizontal_wrapped` work better with text.
+        // egui::Window::new("Edit or Insert an Entry").default_pos([720.0, 480.0]).show(ctx, |ui| {
+        //     ui.style_mut().spacing.interact_size.y = 0.0; // hack to make `horizontal_wrapped` work better with text.
 
-            ui.label("We use STFU-8 as a hacky text encoding/decoding protocol for data that might be not quite UTF-8 but is still mostly UTF-8. \
-            It is based on the syntax of the repr created when you write (or print) binary text in python, C or other common programming languages.");
+        //     ui.label("We use STFU-8 as a hacky text encoding/decoding protocol for data that might be not quite UTF-8 but is still mostly UTF-8. \
+        //     It is based on the syntax of the repr created when you write (or print) binary text in python, C or other common programming languages.");
 
-            ui.add_space(8.0);
+        //     ui.add_space(8.0);
 
-            ui.label("Basically STFU-8 is the text format you already write when use escape codes in C, python, rust, etc. \
-            It permits binary data in UTF-8 by escaping them with \\, for instance \\n and \\x0F.");
+        //     ui.label("Basically STFU-8 is the text format you already write when use escape codes in C, python, rust, etc. \
+        //     It permits binary data in UTF-8 by escaping them with \\, for instance \\n and \\x0F.");
 
-            ui.add_space(8.0);
+        //     ui.add_space(8.0);
 
-            ui.horizontal_wrapped(|ui| {
-                ui.spacing_mut().item_spacing.x = 0.0;
-                ui.label("More about how we interpret encoding/decoding ");
-                ui.hyperlink_to("on the stfu8 documentation", "https://docs.rs/stfu8");
-                ui.label(".");
-            });
+        //     ui.horizontal_wrapped(|ui| {
+        //         ui.spacing_mut().item_spacing.x = 0.0;
+        //         ui.label("More about how we interpret encoding/decoding ");
+        //         ui.hyperlink_to("on the stfu8 documentation", "https://docs.rs/stfu8");
+        //         ui.label(".");
+        //     });
 
-            ui.separator();
+        //     ui.separator();
 
-            let EscapedEntry { key, data } = &mut self.entry_to_insert;
-            ui.add(egui::TextEdit::singleline(key).hint_text("escaped key"));
-            ui.add(egui::TextEdit::multiline(data).hint_text("escaped data"));
+        //     let EscapedEntry { key, data } = &mut self.entry_to_insert;
+        //     ui.add(egui::TextEdit::singleline(key).hint_text("escaped key"));
+        //     ui.add(egui::TextEdit::multiline(data).hint_text("escaped data"));
 
-            if ui.button("insert").clicked() {
-                if let Some(wtxn) = self.wtxn.as_mut() {
-                    let key = self.entry_to_insert.decoded_key().unwrap();
-                    let data = self.entry_to_insert.decoded_data().unwrap();
-                    self.database.1.put(wtxn, &key, &data).unwrap();
-                    self.entry_to_insert.clear();
-                }
-            }
+        //     if ui.button("insert").clicked() {
+        //         if let Some(wtxn) = self.wtxn.as_mut() {
+        //             let key = self.entry_to_insert.decoded_key().unwrap();
+        //             let data = self.entry_to_insert.decoded_data().unwrap();
+        //             self.database.1.put(wtxn, &key, &data).unwrap();
+        //             self.entry_to_insert.clear();
+        //         }
+        //     }
 
-            if ui.button("delete").clicked() {
-                if let Some(wtxn) = self.wtxn.as_mut() {
-                    let key = self.entry_to_insert.decoded_key().unwrap();
-                    self.database.1.delete(wtxn, &key).unwrap();
-                    self.entry_to_insert.clear();
-                }
-            }
-        });
+        //     if ui.button("delete").clicked() {
+        //         if let Some(wtxn) = self.wtxn.as_mut() {
+        //             let key = self.entry_to_insert.decoded_key().unwrap();
+        //             self.database.1.delete(wtxn, &key).unwrap();
+        //             self.entry_to_insert.clear();
+        //         }
+        //     }
+        // });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
-                ui.horizontal(|ui| {
-                    ui.add(
-                        egui::TextEdit::singleline(&mut self.database_to_open)
-                            .hint_text("database name"),
-                    );
-                    if ui.button("open").clicked() {
-                        let env = ENV.wait();
-                        let database_name = if self.database_to_open.is_empty() {
-                            None
-                        } else {
-                            Some(mem::take(&mut self.database_to_open))
-                        };
-
-                        // If there is a write txn opened, use it, else use a new read txn.
-                        let long_rtxn;
-                        let rtxn;
-                        match self.wtxn.as_ref() {
-                            Some(wtxn) => rtxn = wtxn.deref(),
-                            None => {
-                                long_rtxn = self.env.read_txn().unwrap();
-                                rtxn = &long_rtxn;
-                            }
-                        };
-
-                        let db = env
-                            .open_database(&rtxn, database_name.as_ref().map(AsRef::as_ref))
-                            .unwrap()
-                            .unwrap();
-                        self.database = (database_name, db);
-                    }
-                });
-
-                ui.separator();
-
                 let button = if self.wtxn.is_some() {
-                    egui::Button::new("start writing").fill(Color32::DARK_GREEN)
+                    egui::Button::new("start writing").fill(Color32::GREEN)
                 } else {
-                    egui::Button::new("start writing").fill(Color32::DARK_RED)
+                    egui::Button::new("start writing").fill(Color32::RED)
                 };
 
                 if ui.add(button).clicked() && self.wtxn.is_none() {
@@ -175,73 +156,177 @@ impl eframe::App for LmdbEditor {
                 }
             });
 
-            ui.separator();
+            let mut behavior = TreeBehavior {};
+            self.tree.ui(&mut behavior, ui);
 
-            // If there is a write txn opened, use it, else use a new read txn.
-            let long_rtxn;
-            let rtxn;
-            match self.wtxn.as_ref() {
-                Some(wtxn) => rtxn = wtxn.deref(),
-                None => {
-                    long_rtxn = self.env.read_txn().unwrap();
-                    rtxn = &long_rtxn;
+            // Automatically insert an OpenNew Tab when one is missing
+            if let Some(root) = self.tree.root() {
+                let must_insert = match self.tree.tiles.get(root).unwrap() {
+                    Tile::Container(Container::Tabs(t)) => t
+                        .children
+                        .iter()
+                        .find(|&&t| {
+                            self.tree
+                                .tiles
+                                .get(t)
+                                .map_or(true, |t| matches!(t, Tile::Pane(p) if p.is_open_new()))
+                        })
+                        .is_none(),
+                    _ => false,
+                };
+
+                if must_insert {
+                    let tid = self
+                        .tree
+                        .tiles
+                        .insert_pane(Pane::OpenNew { database_to_open: String::new() });
+                    if let Tile::Container(Container::Tabs(t)) =
+                        self.tree.tiles.get_mut(root).unwrap()
+                    {
+                        t.children.push(tid);
+                    }
                 }
-            };
-
-            let num_rows = self.database.1.len(&rtxn).unwrap().try_into().unwrap();
-            let mut prev_row_index = None;
-            let mut iter = self.database.1.iter(&rtxn).unwrap();
-
-            TableBuilder::new(ui)
-                .column(Column::auto().resizable(true))
-                .column(Column::auto().resizable(true))
-                .column(Column::remainder())
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.label("Keys");
-                    });
-                    header.col(|ui| {
-                        ui.label("Values");
-                    });
-                    header.col(|ui| {
-                        ui.label("Operations");
-                    });
-                })
-                .body(|body| {
-                    body.rows(30.0, num_rows, |row_index, mut row| {
-                        assert!(prev_row_index.map_or(true, |p| p + 1 == row_index));
-                        if prev_row_index.is_none() {
-                            let _ = iter.by_ref().skip(row_index).next();
-                            // iter.advance_by(row_index).unwrap();
-                        }
-                        prev_row_index = Some(row_index);
-
-                        if let Some(result) = iter.next() {
-                            let (key, data) = result.unwrap();
-                            let encoded_key = stfu8::encode_u8_pretty(key);
-                            let encoded_data = stfu8::encode_u8_pretty(data);
-
-                            row.col(|ui| {
-                                ui.label(&encoded_key);
-                            });
-                            row.col(|ui| {
-                                ui.label(&encoded_data);
-                            });
-                            row.col(|ui| {
-                                // TODO Replace me by a ✏️
-                                if ui.button("edit").clicked() {
-                                    self.entry_to_insert.key = encoded_key;
-                                    self.entry_to_insert.data = encoded_data;
-                                }
-                                // // Replace me by a red 🗑️
-                                // if ui.button("delete").clicked() {
-                                //     if let Some(wtxn) = self.wtxn.as_mut() {
-                                //     }
-                                // }
-                            });
-                        }
-                    });
-                });
+            }
         });
+    }
+}
+
+enum Pane {
+    Editable {
+        database_name: Option<String>,
+        database: Database<ByteSlice, ByteSlice>,
+        entry_to_insert: EscapedEntry,
+    },
+    OpenNew {
+        database_to_open: String,
+    },
+}
+
+impl Pane {
+    fn is_open_new(&self) -> bool {
+        matches!(self, Pane::OpenNew { .. })
+    }
+}
+
+struct TreeBehavior {}
+
+impl egui_tiles::Behavior<Pane> for TreeBehavior {
+    fn tab_title_for_pane(&mut self, pane: &Pane) -> egui::WidgetText {
+        match pane {
+            Pane::Editable { database_name: Some(name), .. } => format!("{name}").into(),
+            Pane::Editable { database_name: None, .. } => format!("{{main}}").into(),
+            Pane::OpenNew { .. } => format!("Open new").into(),
+        }
+    }
+
+    fn pane_ui(
+        &mut self,
+        ui: &mut egui::Ui,
+        _tile_id: egui_tiles::TileId,
+        pane: &mut Pane,
+    ) -> egui_tiles::UiResponse {
+        // If there is a write txn opened, use it, else use a new read txn.
+        let env = ENV.wait();
+        let long_rtxn;
+        let rtxn;
+        // match self.wtxn.as_ref() {
+        //     Some(wtxn) => rtxn = wtxn.deref(),
+        //     None => {
+        long_rtxn = env.read_txn().unwrap();
+        rtxn = &long_rtxn;
+        //     }
+        // };
+
+        ui.add_space(5.0);
+
+        match pane {
+            Pane::Editable { database, entry_to_insert, .. } => {
+                let num_rows = database.len(&rtxn).unwrap().try_into().unwrap();
+                let mut prev_row_index = None;
+                let mut iter = database.iter(&rtxn).unwrap();
+
+                TableBuilder::new(ui)
+                    .column(Column::auto().resizable(true))
+                    .column(Column::auto().resizable(true))
+                    .column(Column::remainder())
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.label("Keys");
+                        });
+                        header.col(|ui| {
+                            ui.label("Values");
+                        });
+                        header.col(|ui| {
+                            ui.label("Operations");
+                        });
+                    })
+                    .body(|body| {
+                        body.rows(30.0, num_rows, |row_index, mut row| {
+                            assert!(prev_row_index.map_or(true, |p| p + 1 == row_index));
+                            if prev_row_index.is_none() {
+                                let _ = iter.by_ref().skip(row_index).next();
+                                // iter.advance_by(row_index).unwrap();
+                            }
+                            prev_row_index = Some(row_index);
+
+                            if let Some(result) = iter.next() {
+                                let (key, data) = result.unwrap();
+                                let encoded_key = stfu8::encode_u8_pretty(key);
+                                let encoded_data = stfu8::encode_u8_pretty(data);
+
+                                row.col(|ui| {
+                                    ui.label(&encoded_key);
+                                });
+                                row.col(|ui| {
+                                    ui.label(&encoded_data);
+                                });
+                                row.col(|ui| {
+                                    // TODO Replace me by a ✏️
+                                    if ui.button("edit").clicked() {
+                                        entry_to_insert.key = encoded_key;
+                                        entry_to_insert.data = encoded_data;
+                                    }
+                                    // // Replace me by a red 🗑️
+                                    // if ui.button("delete").clicked() {
+                                    //     if let Some(wtxn) = self.wtxn.as_mut() {
+                                    //     }
+                                    // }
+                                });
+                            }
+                        });
+                    });
+            }
+            Pane::OpenNew { database_to_open } => {
+                let response = ui.horizontal(|ui| {
+                    ui.add(egui::TextEdit::singleline(database_to_open).hint_text("database name"));
+                    if ui.button("open").clicked() {
+                        let env = ENV.wait();
+                        let database_name = if database_to_open.is_empty() {
+                            None
+                        } else {
+                            Some(mem::take(database_to_open))
+                        };
+
+                        let database = env
+                            .open_database(&rtxn, database_name.as_ref().map(AsRef::as_ref))
+                            .unwrap()
+                            .unwrap();
+                        Some(Pane::Editable {
+                            database_name,
+                            database,
+                            entry_to_insert: Default::default(),
+                        })
+                    } else {
+                        None
+                    }
+                });
+
+                if let InnerResponse { inner: Some(p), .. } = response {
+                    *pane = p;
+                }
+            }
+        }
+
+        egui_tiles::UiResponse::None
     }
 }
